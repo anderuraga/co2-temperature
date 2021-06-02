b42a583d1e694e1fc5a850616dd8566eaef2558b
ander.uraga@gmail.com <ander.uraga@gmail.com>
2021-06-02 13:45:26 +0200

    ams cambios

---
+++ ardunino/script_post_data.ino
@@ -0,0 +1,134 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include "MHZ19.h"
+#include <SoftwareSerial.h>                                // Remove if using HardwareSerial
+
+#define RX_PIN D2                                        // Rx pin which the MHZ19 Tx pin is attached to
+#define TX_PIN D1                                    // Tx pin which the MHZ19 Rx pin is attached to
+#define BAUDRATE 9600                                      // Device to MH-Z19 Serial baudrate (should not be changed)
+
+MHZ19 myMHZ19;                                             // Constructor for library
+SoftwareSerial mySerial(RX_PIN, TX_PIN);                   // (Uno example) create device to MH-Z19 serial
+unsigned long getDataTimer = 0;
+
+//-------------------VARIABLES GLOBALES--------------------------
+int contconexion = 0;
+//int tiempoEnvio = 1 * 60 * 1000; // 1 min
+int tiempoEnvio =  10 * 1000; // 10 seg
+
+const char *ssid = "Elorrieta_ethazi";
+const char *password = "123ETHAZI";
+unsigned long previousMillis = 0;
+
+char host[48];
+String strhost = "10.0.21.15"; // ip del pc donde está la base de datos
+String strurl = "/tutorial/enviardatos.php";
+String chipid = "";
+
+//-------Función para Enviar Datos a la Base de Datos SQL--------
+
+String enviardatos(String datos) {
+
+
+  Serial.println (
+    String("POST ") + strurl + " HTTP/1.1" + "\r\n" +
+    "Host: " + strhost + "\r\n" +
+    "Accept: */*" + "*\r\n" +
+    "Content-Length: " + datos.length() + "\r\n" +
+    "Content-Type: application/x-www-form-urlencoded" + "\r\n" +
+    "\r\n" + datos
+  );
+
+  String linea = "error";
+  WiFiClient client;
+  strhost.toCharArray(host, 49);
+  if (!client.connect(host, 80)) {
+    Serial.println("Fallo de conexion");
+    return linea;
+  }
+
+
+
+
+  client.print(String("POST ") + strurl + " HTTP/1.1" + "\r\n" +
+               "Host: " + strhost + "\r\n" +
+               "Accept: */*" + "*\r\n" +
+               "Content-Length: " + datos.length() + "\r\n" +
+               "Content-Type: application/x-www-form-urlencoded" + "\r\n" +
+               "\r\n" + datos);
+  delay(10);
+
+  Serial.print("Enviando datos a SQL...");
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println("Cliente fuera de tiempo!");
+      client.stop();
+      return linea;
+    }
+  }
+  // Lee todas las lineas que recibe del servidro y las imprime por la terminal serial
+  while (client.available()) {
+    linea = client.readStringUntil('\r');
+  }
+  Serial.println(linea);
+  return linea;
+}
+
+//-------------------------------------------------------------------------
+
+void setup() {
+  Serial.begin(9600);                                     // Device to serial monitor feedback
+
+  mySerial.begin(BAUDRATE);                               // (Uno example) device to MH-Z19 serial start
+  myMHZ19.begin(mySerial);                                // *Serial(Stream) refence must be passed to library begin().
+
+  myMHZ19.autoCalibration();                              // Turn auto calibration ON (OFF autoCalibration(false))
+
+  Serial.println("");
+
+  Serial.print("chipId: ");
+  chipid = String(ESP.getChipId());
+  Serial.println(chipid);
+
+  // Conexión WIFI
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED and contconexion < 50) { //Cuenta hasta 50 si no se puede conectar lo cancela
+    ++contconexion;
+    delay(500);
+    Serial.print(".");
+  }
+  if (contconexion < 50) {
+    Serial.println("");
+    Serial.println("WiFi conectado");
+    Serial.println(WiFi.localIP());
+  }
+  else {
+    Serial.println("");
+    Serial.println("Error de conexion");
+  }
+}
+
+//--------------------------LOOP--------------------------------
+void loop() {
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= tiempoEnvio ) {
+    previousMillis = currentMillis;
+    //int analog = analogRead(0); //initializes/defines the output pin of the LM35 temperature sensor
+    //float temp = analog*0.322265625;
+    //Serial.println(temp);
+    String co2 = "";
+    //int co2;
+
+    int8_t temp;
+    temp = myMHZ19.getTemperature();                     // Request Temperature (as Celsius)
+    //Serial.print("Temperature (C): ");
+    //Serial.println(temp);
+
+    co2 = String(myMHZ19.getCO2());
+    enviardatos("chipid=" + chipid + "&co2=" + co2 + "&temp=" + temp);
+  }
+}
--- web/index.php
+++ web/index.php
@@ -29,247 +29,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sensor Co2 & Temp</title>
+
+    <link rel="stylesheet" href="css/styles.css">
+
   </head>
-  
-  
-  <style>
-  @import url(https://fonts.googleapis.com/css?family=Roboto:400,300);
-html,
-body {
-    background-color: #F3F3F3;
-    font-family: 'Roboto', sans-serif;
-}
-
-.card {
-    margin: 0 auto;   
-    padding: 30px;
-    width: 290px;    
-    border-radius: 15px;
-    background-color: #fff;
-    box-shadow: 1px 2px 10px rgb(0 0 0 / 20%);
-    -webkit-animation: open 2s cubic-bezier(.39, 0, .38, 1);
-}
-
-@-webkit-keyframes open {
-    from {
-        padding: 0 30px;
-        height: 0;
-    }
-    to {
-        height: 470px;
-    }
-}
-
-h1,
-h2,
-h3,
-h4 {
-    position: relative;
-}
-
-h1 {
-    float: right;
-    color: #666;
-    font-weight: 300;
-    font-size: 6.59375em;
-    line-height: .2em;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .2s;
-}
-
-h2 {
-    font-weight: 300;
-    font-size: 2.25em;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1);
-}
-
-h3 {
-    float: left;
-    margin-right: 33px;
-    color: #777;
-    font-weight: 400;
-    font-size: 1em;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .1s;
-}
-
-.container {
-	display: flex;	
-	max-width: 1024px;
-	margin: auto;
-}
-
-@media (max-width: 425px) {
-	.container {
-		flex-direction: column;
-	}
-	.card {
-		margin-bottom: 2rem;
-	}		
-}
-
-span {
-    margin-left: 24px;
-    color: #999;
-    font-weight: 300;
-}
-
-span span {
-    margin-left: 0;
-}
-
-.dot {
-    font-size: .9em;
-}
-
-.co2{
-	color: #433e3e;
-    position: relative;
-    top: 40px;
-    font-size: 30px;
-    z-index: 66;
-    font-weight: 800;
-    left: 30px;
-	 -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .2s;
-}
-
-.sky {
-    position: relative;
-    margin-top: 108px;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    background-color: #03A9F4;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .2s;
-}
-
-.tempback {
-    position: relative;
-    margin-top: 108px;
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    background-color: #f49b03;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .2s;
-}
-
-.thermometer {
-	position: relative;
-    height: 80px;
-    top: 10px;
-    left: 12px;
-}
-
-.sun {
-    position: relative;
-    top: -3px;
-    width: 55px;
-    height: 55px;
-    border-radius: 50%;
-    background-color: #FFEB3B;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .5s;
-}
-
-.cloud {
-    position: absolute;
-    top: 60px;
-    left: 30px;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .7s;
-}
-
-.cloud:before,
-.cloud:after {
-    position: absolute;
-    display: block;
-    content: "";
-}
-
-.cloud:before {
-    margin-left: -10px;
-    width: 51px;
-    height: 18px;
-    background: #fff;
-}
-
-.cloud:after {
-    position: absolute;
-    top: -10px;
-    left: -22px;
-    width: 28px;
-    height: 28px;
-    border-radius: 50%;
-    background: #fff;
-    box-shadow: 50px -6px 0 6px #fff, 25px -19px 0 10px #fff;
-}
-
-table {
-    position: relative;
-    top: 10px;
-    width: 100%;
-    text-align: center;
-}
-
-tr:nth-child(1) td:nth-child(1),
-tr:nth-child(1) td:nth-child(2),
-tr:nth-child(1) td:nth-child(3),
-tr:nth-child(1) td:nth-child(4),
-tr:nth-child(1) td:nth-child(5) {
-    padding-bottom: 32px;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .7s;
-}
-
-tr:nth-child(2) td:nth-child(1),
-tr:nth-child(2) td:nth-child(2),
-tr:nth-child(2) td:nth-child(3),
-tr:nth-child(2) td:nth-child(4),
-tr:nth-child(2) td:nth-child(5) {
-    padding-bottom: 7px;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .9s;
-}
-
-tr:nth-child(3) td:nth-child(1),
-tr:nth-child(3) td:nth-child(2),
-tr:nth-child(3) td:nth-child(3),
-tr:nth-child(3) td:nth-child(4),
-tr:nth-child(3) td:nth-child(5) {
-    padding-bottom: 7px;
-    -webkit-animation: up 2s cubic-bezier(.39, 0, .38, 1) .9s;
-}
-
-tr:nth-child(2),
-tr:nth-child(3) {
-    font-size: .9em;
-}
-
-tr:nth-child(3) {
-    color: #999;
-}
-
-@-webkit-keyframes up {
-    0% {
-        opacity: 0;
-        -webkit-transform: translateY(15px);
-    }
-    50% {
-        opacity: 0;
-        -webkit-transform: translateY(15px);
-    }
-    99% {
-        -webkit-animation-play-state: paused;
-    }
-    100% {
-        opacity: 1;
-    }
-}
-
-
-
-  </style>
-  
   <body>
-  
 	
-		<a href="mostrar.php">Ver todos los valores</a>
+	<a href="mostrar.php">Ver todos los valores</a>
 		
-		<div class="container">
+	<div class="container">
 		
 		  <div class="card">        
 			<h3>Sensor XXXX<span>Aula 013</h3>
@@ -295,13 +63,13 @@ tr:nth-child(3) {
 				</tr>            
 			</table>
 		</div>
-		
+		<!-- card 1 -->
 		
 		<div class="card">        
 			<h3>Sensor XXXX<span>Aula 013</h3>
 			<h1><?php echo $row2['temp']?>º</h1>
 			<div class="tempback">
-				<img src="thermometer.svg" alt="termometro" class="thermometer">	
+				<img src="images/thermometer.svg" alt="termometro" class="thermometer">	
 			</div>
 			<table>
 				<tr>
@@ -316,11 +84,11 @@ tr:nth-child(3) {
 				</tr>            
 			</table>
 		</div>
+        <!-- card 2 -->
 		
-		</div>
+	</div>
+    <!-- div.container -->
 		
-	
-
 
 </table>
 
