eb2a271aefa296650644ecb9e3247f774a038738
ander.uraga@gmail.com <ander.uraga@gmail.com>
2021-06-04 12:30:24 +0200

    grafico funcionando pero muchos valores

--- web/vista/mostrar.php
+++ web/vista/mostrar.php
@@ -6,7 +6,7 @@
 	$fecha1 = $_GET['f1'];
 	$fecha2 = $_GET['f2'];
 
-	$filtro = "Ultimos 500 registros";
+	$filtro = "Últimos Registros";
 	if ( !is_null($fecha1) )
 	{
 		$filtro = "Registros desde ".$fecha1." hasta ".$fecha2;
@@ -20,6 +20,8 @@
 
 	// echo($sql);
 	$resultado = mysqli_query($conn, $sql);
+
+
 			
 	
 ?>
@@ -40,7 +42,9 @@
   <body class="container">
 	<a href="../index.php" class="btn btn-outline-primary mb-4 mt-4">Volver</a>
   
-	<h3><?php echo($filtro)?></h3>
+	<h3><span class="bg-secondary text-light p-1"><?php echo($resultado->num_rows)?></span> <?php echo($filtro)?></h3>
+	
+	<!-- filtro fechas -->
 		<form method="get" action="mostrar.php" class="form-inline mb-4 bg-light p-2" >
 			<div class="row">
 				<div class="col">				
@@ -60,36 +64,63 @@
 			</div>
 		</form>
 	
-	<!-- Row -->
+	<div class="row">
+		<!-- tabla -->
+		<div class="col">
+			<table id="myTable" class="table table-striped table-hover">
+			<caption>Datos Co2 - Aula X</caption>
+			<thead>
+				<tr>
+				<th>#</th>
+				<th>co2</th>
+				<th>temp</th>
+				<th>fecha</th>
+				</tr>
+			</thead>
+				<tbody>
+				
+				<?php
+					
+					// variables para los graficos
+					$dataCount = $resultado->num_rows;
+					$labelsTemp = [];
+					$dataTemp = [];
+					$cont = 0;
+					
+					while($row = mysqli_fetch_assoc($resultado)) {
 
-	<table id="myTable" class="table table-striped table-hover">
-	<caption>Datos Co2 - Aula X</caption>
-	<thead>
-		<tr>
-		<th>#</th>
-		<th>co2</th>
-		<th>temp</th>
-		<th>fecha</th>
-		</tr>
-	</thead>
-		<tbody>
-		
-		<?php
-			
-			
-			while($row = mysqli_fetch_assoc($resultado)) {
-				echo " <tr>";
-					echo "<td>".$row['chipId']."</td>";
-					echo "<td>".$row['co2']."</td>";
-					echo "<td>".$row['temp']."</td>";
-					echo "<td>".$row['fecha']."</td>";
-				echo " </tr>";	
-			}
-			
-		?>
 
-		</tbody>
-	</table>
+						// rellenar datos para graficos
+						$labelsTemp[$cont] = $row['fecha'];
+					    $dataTemp[$cont] = $row['temp'];
+
+
+						// pintar TABLA HTML
+						echo " <tr>";
+							echo "<td>".$row['chipId']."</td>";
+							echo "<td>".$row['co2']."</td>";
+							echo "<td>".$row['temp']."</td>";
+							echo "<td>".$row['fecha']."</td>";
+						echo " </tr>";	
+
+						$cont++;
+					}
+					
+				?>
+
+				</tbody>
+			</table>
+		</div>
+
+		<!-- grafico -->
+		<div class="col">
+			<canvas id="myChart" width="400" height="300"></canvas>		
+		</div>	
+
+	</div>
+	<!-- /row -->
+
+	
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -101,9 +132,50 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
 	<script src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
-		
+
+	
+	<script src="https://raw.githubusercontent.com/chartjs/Chart.js/master/docs/scripts/utils.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.3.2/chart.js"></script>
+	
 	<script src="../resources/js/main.js"></script>
-   
+  
+	<script>
+
+			const DATA_COUNT = <?php echo $dataCount?>;
+			const NUMBER_CFG = {count: DATA_COUNT, min: 0, max: 40};
+
+			const labels = <?php echo json_encode($labelsTemp)?>;
+			const data = {
+			labels: labels,
+			datasets: [				
+				{
+				label: 'Temperatura',
+				data: <?php echo json_encode($dataTemp)?>,
+				borderColor: 'rgba(255, 99, 132)',
+				backgroundColor: 'rgba(255, 99, 132, 0.2)',
+				}
+			]
+			};
+
+			var ctx = document.getElementById('myChart').getContext('2d');
+			var myChart = new Chart(ctx, {
+				type: 'line',
+				options: {
+					responsive: true,
+					plugins: {
+					legend: {
+						position: 'top',
+					},
+					title: {
+						display: true,
+						text: '<?php echo $dataCount?> Temperaturas pintadas'
+					}
+					}
+				},
+				data: data
+			});
+			</script>
+
   </body>
 </html>
 <?php
